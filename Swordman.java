0044105ac1d747330f091b7ecbeecfa98fe69882
Fada1007 <32326973+Fada1007@users.noreply.github.com>
2019-02-20 11:58:49 +0700

    Add files via upload

---
+++ Swordman.java
@@ -0,0 +1,17 @@
+public class Swordman {
+    private int hp;
+    private int exp;
+    private int level;
+    private int maxexp;
+    private Bags bag;
+
+    public Swordman(String value) {
+        this.hp = 100;
+        this.exp = 0;
+        this.maxexp = 50;
+        this.level = 0;
+        this.attack = 10;
+
+    }
+
+}
